6d915dac69536b69ced533b2407feff7ed96b832
Phan Tấn Tài <lonelyguitarist2745@gmail.com>
2026-05-09 08:56:38 +0700

    feat(auth): add /login and /register route pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { AuthPage, LoginForm } from "@/features/auth"
+
+export default function LoginPage(): React.JSX.Element {
+  return (
+    <AuthPage>
+      <LoginForm />
+    </AuthPage>
+  )
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { AuthPage, RegisterForm } from "@/features/auth"
+
+export default function RegisterPage(): React.JSX.Element {
+  return (
+    <AuthPage>
+      <RegisterForm />
+    </AuthPage>
+  )
+}
